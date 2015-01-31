a095fa9317c3311a586c98f171a665b7ff06a6b2
voxels <developer@noisederived.com>
2015-01-31 16:24:52 -0500

    Painting keypoints into scene

--- LivePhysics/OutScene.m
+++ LivePhysics/OutScene.m
@@ -9,6 +9,8 @@
 #import "OutScene.h"
 #import "OCVCaptureModel.h"
 #import "CaptureTextureModel.h"
+#import "Keypoint.h"
+
 
 @interface OutScene ()
 
@@ -27,32 +29,39 @@
 - (void) didMoveToView:(SKView *)view
 {
     self.viewRef = view;
-//    [self setupRootNode];
+    [self setupRootNode];
     [self setupCameraTextureSprite];
 }
 
 - (void) setupRootNode
 {
     self.rootNode = [[SKEffectNode alloc] init];
-    [self.rootNode setShouldEnableEffects:NO];
-    [self.rootNode setShouldRasterize:YES];
 }
 
 - (void) setupCameraTextureSprite
 {
     self.textureModel = [CaptureTextureModel sharedModel];
     self.cameraTextureSprite = [SKSpriteNode spriteNodeWithTexture:self.textureModel.cameraTexture size:self.viewRef.bounds.size];
-
     self.cameraTextureSprite.size = CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height);
     self.cameraTextureSprite.position = CGPointMake(self.view.bounds.size.width/2.f, self.view.bounds.size.height / 2.f);
-
+    self.cameraTextureSprite.zPosition = 0;
     [self addChild:self.cameraTextureSprite];
-//    [self.rootNode addChild:self.cameraTextureSprite];
+    [self addChild:self.rootNode];
 }
 
 - (void) addKeyPoints:(NSArray *) keypoints
 {
-//    NSLog(@"Keypoints: %lu", keypoints.count);
+    for( Keypoint *thisKeypoint in keypoints )
+    {
+//        Keypoint *thisKeypoint = [keypoints firstObject];
+        SKShapeNode *keypointNode = [SKShapeNode shapeNodeWithCircleOfRadius:2.0];
+        keypointNode.fillColor = [SKColor redColor];
+        keypointNode.lineWidth = 0;
+        keypointNode.position = CGPointMake( self.view.bounds.size.width - thisKeypoint.pt.x * 800.f/640.f, self.view.bounds.size.height - thisKeypoint.pt.y * 600.f/480.f);
+        keypointNode.zPosition = 3;
+    
+        [self.rootNode addChild:keypointNode];
+    }
 }
 
 
@@ -68,7 +77,6 @@
 //    self.renderedNode.texture = self.saveTexture;
 //    if( !_renderedNode )
 //    {
-//        NSLog(@"adding root node");
 //        self.renderedNode = [[SKSpriteNode alloc] initWithTexture:self.saveTexture];
 //        self.renderedNode.size = CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height);
 //        self.renderedNode.position = CGPointMake(self.view.bounds.size.width/2.f, self.view.bounds.size.height / 2.f);
